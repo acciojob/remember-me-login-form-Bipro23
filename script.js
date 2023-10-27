159b7ba6aad42564c3d390b7bc235e5910190c9f
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-10-27 18:17:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
-//your JS code here. If required.
-const form = document.querySelector('form');
-const usernameInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
-const rememberCheckbox = document.getElementById('checkbox');
-const existingButton = document.getElementById('existing');
+// const form = document.querySelector('form');
+// const usernameInput = document.getElementById('username');
+// const passwordInput = document.getElementById('password');
+// const rememberCheckbox = document.getElementById('checkbox');
+// const existingButton = document.getElementById('existing');
 
-// On page load, check if there are stored login details
-if (localStorage.getItem('username') && localStorage.getItem('password')) {
-  // Populate the input fields with the stored login details
+// // On page load, check if there are stored login details
+// if (localStorage.getItem('username') && localStorage.getItem('password')) {
+// // Populate the input fields with the stored login details
 
-  // Show the "Login as existing user" button
-	existingButton.style.visibility = "visible"
-}
-
+// // Show the "Login as existing user" button
+// existingButton.style.visiblity = "visible"
+// }
 
 // Add an event listener to the form on submit
 form.addEventListener('submit', function(e) {
-  e.preventDefault(); // Prevent the form from submitting
+e.preventDefault(); // Prevent the form from submitting
 
-  const username = usernameInput.value;
-  const password = passwordInput.value;
+const username = usernameInput.value;
+const password = passwordInput.value;
 
-  // Check if the "remember me" checkbox is checked
-  if (rememberCheckbox.checked) {
-    // Store the username and password in local storage
-    localStorage.setItem('username', username);
-    localStorage.setItem('password', password);
-  } else {
-    // Remove the stored username and password from local storage
-    localStorage.removeItem('username');
-    localStorage.removeItem('password');
-  }
+// Check if the "remember me" checkbox is checked
+if (rememberCheckbox.checked) {
+// Store the username and password in local storage
+localStorage.setItem('username', username);
+localStorage.setItem('password', password);
+} else {
+// Remove the stored username and password from local storage
+localStorage.removeItem('username');
+localStorage.removeItem('password');
+}
 
-  // TODO: Add your login logic here
-	alert(`Logged in as ${usernameInput.value}`)
+// TODO: Add your login logic here
+alert(Logged in as ${usernameInput.value})
 
-  // Show the "Login as existing user" button
-	existingButton.style.display = "block"
+// Show the "Login as existing user" button
+existingButton.style.visibility = 'visible';
 });
